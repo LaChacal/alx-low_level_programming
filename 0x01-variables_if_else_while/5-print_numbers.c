35e637ed592ac6afe488e119711c84528a0f3e04
LaChacal <you@example.com>
2022-12-11 04:12:13 -0800

    prints all base 10 numbers 0to9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (good)
+ */
+
+int main(void)
+
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
