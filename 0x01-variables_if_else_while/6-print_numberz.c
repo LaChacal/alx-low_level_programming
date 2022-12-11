cac0e70c9124ce8eea42a24dc29006ad14466548
LaChacal <you@example.com>
2022-12-11 04:22:02 -0800

    print all base 10 num using int instead of char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (good)
+ */
+
+int main(void)
+
+{
+	int c = '0';
+
+	while (c < '10')
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
