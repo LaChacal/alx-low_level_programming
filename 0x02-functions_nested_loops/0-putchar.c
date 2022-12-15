7f078b2b71519b2e1b0e4f37206703674686fca2
LaChacal <you@example.com>
2022-12-14 16:14:34 -0800

    print txt and new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.>
+#include "main.h"
 
 /**
  * main - check the code
@@ -8,7 +8,14 @@
 
 int main(void)
 {
-	printf("_putchar"'\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
