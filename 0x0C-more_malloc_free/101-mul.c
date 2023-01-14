8118ebc7193b996d4161f312d02e02c8632db0bf
LaChacal <you@example.com>
2023-01-13 20:56:45 -0800

    mult two numbs

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,12 +20,18 @@ int i, j;
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
+
 			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{  printf("Error\n");
 			exit(98); }
+
 		}
+
 	}
+
 	mul = atol(argv[1]) *  atol(argv[2]);
+
 	printf("%lu\n", mul);
+
 return (0);
 }
